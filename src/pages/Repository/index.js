89b95821782c85317bc2e5af9c08d02c05e989ce
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-02-16 00:32:44 -0300

    Exibindo repository e issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
+import { Loading, Owner, IssueList } from './styles';
 
-// import { Container } from './styles';
+import Container from '../../components/Container';
 
 export default function Repository(props) {
   const [repository, setRepository] = useState({});
@@ -9,22 +12,57 @@ export default function Repository(props) {
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    (async function callApi() {
+    async function callApi() {
       const repoName = decodeURIComponent(props.match.params.repository);
-      const data = await Promise.all([
+      const response = await Promise.all([
         api.get(`/repos/${repoName}`),
         api.get(`/repos/${repoName}/issues`, {
           params: { state: 'open', per_page: 5 },
         }),
       ]);
 
-      const [repoRequested, issuesRequested] = data;
+      const [repoRequested, issuesRequested] = response;
 
-      setRepository(repoRequested);
-      setIssues(issuesRequested);
+      setRepository(repoRequested.data);
+      setIssues(issuesRequested.data);
       setLoading(false);
-    })();
-  });
+    }
 
-  return <h1>Repository</h1>;
+    callApi();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (isLoading) {
+    return <Loading>Carregando...</Loading>;
+  }
+  return (
+    <Container>
+      <Owner>
+        <Link to="/">Voltar aos repositórios</Link>
+        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+      </Owner>
+      <IssueList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssueList>
+    </Container>
+  );
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({ repository: PropTypes.string }).isRequired,
+};
